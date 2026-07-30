4bf4e2f52739d248c283024d602e650741e691cf
Dexter <dexben5@gmail.com>
2026-07-30 15:34:25 -0500

    Update homepage intro and make maze blend in

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,32 @@
-import { getSortedPostsData } from "@lib/posts";
+import {getSortedPostsData} from "@lib/posts";
 import Link from "next/link";
 import DateFormat from "@components/DateFormat";
 
-// argument passed can also be expressed as props, then props.allPostsData within
 export default async function Page() {
   const allPostsData = await getSortedPostsData();
 
-  // Everything within layout is its children because it's not self-closing
   return (
     <>
-      <section className="text-[1.2rem] leading-normal">
+      <section className="text-[1.2rem] leading-normal space-y-4">
         <p>
-          My name is Dexter. I like to ski, lift weights, and I'm working to
-          become a full-stack developer.
+          My name is Dexter. I'm currently an undergraduate student at the University of Wisconsin–Madison studying
+          Computer Sciences and Mathematics. I like to ski, lift weights, and I'm working to become a full-stack
+          developer.
         </p>
         <p>
-          On this website you'll see a showcase of my skills, ranging from
-          development with Next.js and React to algorithms implemented in Java.
+          This website is a showcase for my software engineering work as I progress through college and gain
+          professional experience. You can expect to find work in areas such as web development with Next.js and React,
+          applications created in Java, all created with industry-standard tooling.
         </p>
       </section>
       <section className="text-[1.2rem] leading-normal pt-px">
         <h2 className="text-2xl font-extrabold leading-[1.4] my-4">Blog</h2>
         <ul>
-          {allPostsData.map(({ id, date, title }) => (
+          {allPostsData.map(({id, date, title}) => (
             <li className="pb-5" key={id}>
               <Link className="text-blue-600" href={`/posts/${id}`}>
                 {title}
-              </Link>
-              <br />
-              <small className="text-stone-500">
-                <DateFormat dateString={date} />
-              </small>
+              </Link> <br /> <small className="text-stone-500"> <DateFormat dateString={date} /> </small>
             </li>
           ))}
         </ul>
