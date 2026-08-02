6b4c70caf037fbd70cbfd68e63c92604205b8569
Dexter <dexben5@gmail.com>
2026-08-02 17:57:21 -0500

    Add links to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,42 @@
+"use client";
+
+import { IconContext } from "react-icons";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import Link from "next/link";
+
 export default function About() {
-  return <p>This is a page about me</p>;
+  return <Links />;
+}
+
+function Links() {
+  const socialLinks = [
+    { name: "GitHub", href: "https://github.com/dexben5", icon: BsGithub },
+    {
+      name: "LinkedIn",
+      href: "https://linkedin.com/in/dexter-benson",
+      icon: BsLinkedin,
+    },
+  ];
+
+  return (
+    <div className="flex flex-col gap-2 w-fit p-4 bg-gray-300 rounded-2xl">
+      <h1>Links</h1>
+      <IconContext.Provider value={{ size: "36" }}>
+        <div>
+          <ul className="flex space-x-2">
+            {socialLinks.map(({ name, href, icon: Icon }) => (
+              <Link
+                key={name}
+                href={href}
+                target="_blank"
+                className="h-fit w-fit p-2 rounded-lg bg-blue-300"
+              >
+                <Icon />
+              </Link>
+            ))}
+          </ul>
+        </div>
+      </IconContext.Provider>
+    </div>
+  );
 }
